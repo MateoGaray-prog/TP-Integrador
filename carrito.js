a580b0d5a6b611dcb46bd0261905eb8513ffbdaa
MateoGaray-prog <111438006+MateoGaray-prog@users.noreply.github.com>
2023-04-17 23:40:16 -0300

    Update carrito.js

--- carrito.js
+++ carrito.js
@@ -1,6 +1,11 @@
 let productosEnCarrito = localStorage.getItem("productos-en-carrito");
 productosEnCarrito = JSON.parse(productosEnCarrito);
 
+/*ELEMENTOS LLAMADOS DEL DOM*/
+
+const main = document.querySelector("#main");
+const contendorCarrito = document.querySelector("#contenedor-carrito");
+const tituloCarrito = document.querySelector("#titulo-carrito");
 const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
 const contenedorCarritoProductos = document.querySelector("#carrito-productos");
 const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
@@ -9,6 +14,18 @@ let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
 const botonVaciar = document.querySelector("#carrito-acciones-vaciar");
 const contenedorTotal = document.querySelector("#total");
 const botonComprar = document.querySelector("#carrito-acciones-comprar");
+const header = document.querySelector("#header");
+
+// WIZARD
+
+const wizard = document.querySelector("#wizard");
+const grande = document.querySelector(".grande");
+const punto = document.querySelectorAll(".punto");
+const botonCancelar = document.querySelector("#boton-cancelar");
+const botonCancelar2 = document.querySelector("#boton-cancelar-2");
+const botonSiguiente = document.querySelector("#boton-siguiente");
+const botonAtras = document.querySelector("#boton-atras");
+const botonConfirmarCompra = document.querySelector("#boton-confirmar-compra");
 
 function cargarproductosCarrito() {
     if (productosEnCarrito && productosEnCarrito.length > 0){
@@ -93,20 +110,142 @@ function actualizarTotal(){
     total.innerText = `$${totalCalculado}`;
 }
 
+
+
+//-------------------------------------------------------------Wizard ------------------------------------------
+
+
+
+
 botonComprar.addEventListener("click", comprarCarrito);
 function comprarCarrito() {
 
     productosEnCarrito.length = 0;
     localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
-
-    contenedorCarritoVacio.classList.add("disabled");
-    contenedorCarritoProductos.classList.add("disabled");   
-    contenedorCarritoAcciones.classList.add("disabled");
-    contenedorCarritoComprado.classList.remove("disabled"); 
+    
+    tituloCarrito.classList.add("disabled");
+    
+    contendorCarrito.classList.add("disabled");
+    
+    main.classList.add("padding0");
+    wizard.classList.remove("disabled");
+    
+    header.classList.add("blur");
+    header.classList.add("index");
+    header.classList.add("fixed");
 }
 
 
+function cancelar() {
+    wizard.classList.add("disabled");
+    contendorCarrito.classList.remove("disabled");
+    tituloCarrito.classList.remove("disabled");
+    
+    header.classList.remove("blur");
+    header.classList.remove("index");
+    header.classList.remove("fixed");
+    main.classList.remove("padding0");
+}
 
+botonCancelar.addEventListener("click", cancelar);
+botonCancelar2.addEventListener("click", cancelar);
 
+botonSiguiente.addEventListener("click", ()=>{
+    const nombre = document.querySelector("#nombre").value;
+    const apellido = document.querySelector("#apellido").value;
+    const celular = document.querySelector("#celular").value;
+    const email = document.querySelector("#email").value;
 
+    if (nombre === "" || apellido === "" || celular === "" || email === "") {
+        
+        const alertaFormulario = document.querySelector("#alerta-formulario");
+        alertaFormulario.classList.remove("disabled");
 
+        setTimeout(() => {
+            alertaFormulario.classList.add("disabled");   
+        }, 3000);
+
+    } else {
+
+        grande.style.transform = `translateX(-50%)`;
+        botonAtras.classList.remove("activo");
+        botonSiguiente.classList.add("activo");
+
+    }
+});
+
+botonAtras.addEventListener("click", ()=>{
+    grande.style.transform = `translateX(0%)`;
+    botonAtras.classList.add("activo");
+    botonSiguiente.classList.remove("activo");
+
+});
+
+/* 
+botonConfirmarCompra.addEventListener("click", (e)=> {
+    e.preventDefault();
+    const textoCompra = document.querySelector("#texto-compra");
+    const spinner = document.querySelector("#spinner");
+
+    textoCompra.classList.add("disabled");
+    spinner.classList.remove("disabled");
+    botonCancelar2.classList.add("disabled");
+    botonConfirmarCompra.classList.add("disabled");
+    botonAtras.classList.add("ocultar");
+    botonSiguiente.classList.add("ocultar");
+    botonConfirmarCompra.classList.add("disabled");
+    botonCancelar2.classList.add("disabled");
+
+    setTimeout(() => {
+        wizard.classList.add("disabled");
+        header.classList.remove("blur");
+        header.classList.remove("index");
+        header.classList.remove("fixed");
+        main.classList.remove("padding0");
+        contendorCarrito.classList.remove("disabled");
+        tituloCarrito.classList.remove("disabled");
+        vaciarCarrito();
+        contenedorCarritoVacio.classList.add("disabled");
+        contenedorCarritoComprado.classList.remove("disabled");
+    }, 2000);
+    
+})*/
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.classList.add("disabled"); 
+
+   const serviceID = 'default_service';
+   const templateID = 'template_qpml0ck';
+   const textoCompra = document.querySelector("#texto-compra");
+   const spinner = document.querySelector("#spinner");
+
+    textoCompra.classList.add("disabled");
+    spinner.classList.remove("disabled");
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+         
+        spinner.classList.remove("disabled");
+          
+        setTimeout(() => {
+            header.classList.remove("blur");
+            header.classList.remove("index");
+            header.classList.remove("fixed");
+            main.classList.remove("padding0");
+            contendorCarrito.classList.remove("disabled");
+            tituloCarrito.classList.remove("disabled");
+            vaciarCarrito();
+            contenedorCarritoVacio.classList.add("disabled");
+            contenedorCarritoComprado.classList.remove("disabled");
+            wizard.classList.add("opacity");
+        }, 1000);
+    }, (err) => {
+      btn.value = 'REINTENTAR COMPRA';
+        alert(JSON.stringify(err));
+    });
+});
